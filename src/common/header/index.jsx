70096349a8554981e0087b98306a7e382674d5f4
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-08-05 23:10:58 +0900

    SET :: 폴더구조

---
+++ src/common/header/index.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>헤더입니당</div>;
+};
+
+export default Header;
---
+++ src/common/main/index.jsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <div>메인입니당</div>;
+};
+
+export default MainPage;
