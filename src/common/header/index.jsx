06c4cf68e5eedfa8029f5817c845ff17d4f5ccc1
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-08-06 23:54:25 +0900

    헤더 초안

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -1,5 +1,44 @@
+import styled from "styled-components";
+import Logo from "../../components/img/logo.png";
+
 const Header = () => {
-  return <div>헤더입니당</div>;
+  return (
+    <HeaderContainer>
+      <Img src={Logo} />
+      <LogoText>요즘무엇</LogoText>
+      <NavT>로그인</NavT>
+      <NavT>회원가입</NavT>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
+
+const Img = styled.img`
+  width: 78.262px;
+  height: 85.046px;
+`;
+
+const HeaderContainer = styled.div`
+  border: 1px solid black;
+  width: auto;
+  height: 89.578px;
+  box-shadow: 0px 3.062499761581421px 15.312499046325684px 0px
+    rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoText = styled.p`
+  color: #4f98ff;
+  font-size: 36.75px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+const NavT = styled.p`
+  color: #4f98ff;
+`;
